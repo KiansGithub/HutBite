f8e12392065f6bc574be75ef93eb4b018724bd1d
KiansGithub <kiansinnov8@outlook.com>
2025-07-17 12:22:48 +0100

    removed useviewport

--- LoveBites/hooks/useViewportHeight.ts
+++
@@ -1,8 +0,0 @@
-import { useWindowDimensions } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-
-export default function useViewportHeight() {
-    const { height: windowH } = useWindowDimensions();
-    const insets = useSafeAreaInsets();
-    return windowH - insets.top - insets.bottom; 
-}
